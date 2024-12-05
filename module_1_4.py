b271e5d7c7fbc696aa5ed0e3ce859bf9799432e1
TatianaYudina87 <tanyamaz8@gmail.com>
2024-12-05 08:07:38 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+name = input("my_string ")
+text = name
+count = len(text)
+print('Количество символов в тексте: ', count)
+print(name.upper())
+print(name.lower())
+print(name.replace('_', ''))
+print(name[0])
+print(name[-1])
